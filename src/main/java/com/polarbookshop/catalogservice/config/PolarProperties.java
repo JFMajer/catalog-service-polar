34b89b08b009a14d994e33611c1960bc7785f439
JFMajer <jakubmajer@live.com>
2023-04-14 19:54:53 +0200

    missing @Configuration annotation

--- src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
+++ src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
@@ -1,7 +1,9 @@
 package com.polarbookshop.catalogservice.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "polar")
 public class PolarProperties {
     /**
