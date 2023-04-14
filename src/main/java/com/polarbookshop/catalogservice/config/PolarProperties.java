41c982530409c8d8c770d1416907ffbf9f82d5c5
JFMajer <jakubmajer@live.com>
2023-04-14 19:45:54 +0200

    added custom configuration

---
+++ src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
@@ -0,0 +1,20 @@
+package com.polarbookshop.catalogservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "polar")
+public class PolarProperties {
+    /**
+     * The welcome message to be displayed on the home page.
+     */
+
+    private String welcomeMessage;
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+
+    public void setWelcomeMessage(String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
+}
