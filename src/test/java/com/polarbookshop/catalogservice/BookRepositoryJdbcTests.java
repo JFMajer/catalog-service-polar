36f2a1c69d4f7543edbc5fa3c09959ef364aa367
JFMajer <jakubmajer@live.com>
2023-04-16 11:55:17 +0200

    added basic tests for database using testcontainers

---
+++ src/test/java/com/polarbookshop/catalogservice/BookRepositoryJdbcTests.java
@@ -0,0 +1,46 @@
+package com.polarbookshop.catalogservice;
+
+import com.polarbookshop.catalogservice.config.DataConfig;
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJdbcTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("integration")
+public class BookRepositoryJdbcTests {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        var bookIsbn = "978-3-16-148410-0";
+        var book = Book.of(bookIsbn, "The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 10.00);
+        jdbcAggregateTemplate.insert(book);
+        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(bookIsbn);
+    }
+
+    @Test
+    void findBookByIsbnWhenNotExisting() {
+        Optional<Book> actualBook = bookRepository.findByIsbn("1234561238");
+        assertThat(actualBook).isEmpty();
+    }
+}
